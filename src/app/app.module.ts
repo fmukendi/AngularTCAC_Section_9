0af44091aa99523240dc9b2adf0540d789dc24bc
Franck Mukendi <franck.mukendi@gmail.com>
2018-01-26 00:44:41 -0500

    added material design , ngx-bootstrap and calendar Module to this template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MomentModule } from 'angular2-moment/moment.module';
+import {CalendarComponent} from 'ap-angular2-fullcalendar/src/calendar/calendar';
+import { FullCalendarModule } from 'ng-fullcalendar';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatDialogModule , MatButtonModule} from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routedComponents } from './app-routing.module';
@@ -11,7 +24,18 @@ import { AppRoutingModule, routedComponents } from './app-routing.module';
         BrowserModule,
         FormsModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        BsDatepickerModule.forRoot(),
+        ButtonsModule.forRoot(),
+        ModalModule.forRoot(),
+        FlexLayoutModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+        MomentModule ,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
