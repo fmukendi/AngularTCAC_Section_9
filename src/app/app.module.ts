65bc0dd4f989696a2692e35773b5a09fd477b03e
Franck Mukendi <franck.mukendi@gmail.com>
2018-02-12 14:20:31 -0500

    complete module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -23,6 +23,7 @@ import { AppRoutingModule, routedComponents } from './app-routing.module';
     imports: [
         BrowserModule,
         FormsModule,
+        ReactiveFormsModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         BsDatepickerModule.forRoot(),
